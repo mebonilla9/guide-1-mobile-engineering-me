38e675a06148b5d1182b5c30e8fa520bdb711f69
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2022-08-12 11:27:57 -0500

    Modified home view

--- app/src/main/java/co/edu/umb/guide1mobileengineeringme/ui/home/Home.kt
+++ app/src/main/java/co/edu/umb/guide1mobileengineeringme/ui/home/Home.kt
@@ -12,24 +12,28 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import co.edu.umb.guide1mobileengineeringme.Greeting
 import co.edu.umb.guide1mobileengineeringme.R
+import co.edu.umb.guide1mobileengineeringme.ui.theme.Guide1MobileEngineeringMETheme
 
 @Composable
 fun Home() {
   val colorBackground = listOf<Color>(
-    Color(238, 113, 0, 255),
-    Color(101, 0, 126, 255),
-    Color(0, 47, 187)
+    Color(238, 113, 0, 255)//,
+    //Color(101, 0, 126, 255),
+    //Color(0, 47, 187)
   )
 
   Box(
     modifier = Modifier
       .fillMaxSize()
       .background(
-        Brush.verticalGradient(
-          colors = colorBackground
-        )
+        Color(0, 47, 187)
+        /*Brush.linearGradient(
+              colors = colorBackground
+            )*/
       ),
     contentAlignment = Alignment.Center
   ) {
@@ -40,4 +44,10 @@ fun Home() {
       modifier = Modifier.size(400.dp)
     )
   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  Home()
 }
